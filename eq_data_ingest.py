3b2fe3c0df45a1f20c15e025c91fe24d03b93e53
Kathiresan K <kkathir1998@gmail.com>
2026-09-13 20:09:19 +0530

    Update eq_data_ingest.py

--- eq_data_ingest.py
+++ eq_data_ingest.py
@@ -11,7 +11,7 @@ from datetime import datetime, timezone
 print("===== EARTHQUAKE INGESTION DEBUG VERSION =====")
 print("Host configured:", bool(os.environ.get("DATABRICKS_HOST")))
 print("Token configured:", bool(os.environ.get("DATABRICKS_TOKEN")))
-print("Warehouse configured:", bool
+print("Warehouse configured:", bool(os.environ.get("WAREHOUSE_ID")))
 
 DATABRICKS_HOST = os.environ["DATABRICKS_HOST"].rstrip("/")
 DATABRICKS_TOKEN = os.environ["DATABRICKS_TOKEN"]
